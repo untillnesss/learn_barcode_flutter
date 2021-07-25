aba7da9c694dae1d8334335d4855ce4e21cf9d23
Muhammad Abdullah Sa'id <pvzuntil@gmail.com>
2021-07-25 11:05:08 +0700

    add: barcode widget

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,7 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:learn_barcode/app/routes/app_pages.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -28,6 +27,10 @@ class HomeView extends GetView<HomeController> {
             ElevatedButton(
               onPressed: controller.doScan,
               child: Text('Scan QR'),
+            ),
+            BarcodeWidget(
+              barcode: Barcode.qrCode(),
+              data: 'Hello Flutter',
             ),
           ],
         ),
